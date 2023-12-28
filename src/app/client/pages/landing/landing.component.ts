d1a07614fa5b8be973eb281bd80d9e2fda26d1ee
Nguyen Thanh Dat (DEV) <thuykhangvn@gmail.com>
2023-12-28 23:21:00 +0700

    💄Add and update the UI and style files for new feeds

--- src/app/client/pages/landing/landing.component.ts
+++ src/app/client/pages/landing/landing.component.ts
@@ -1,44 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
-import { NewFeed } from 'src/app/core/models';
-import { ShareService } from 'src/app/core/services';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-content',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent implements OnInit, OnDestroy {
-  newFeeds: NewFeed[] | any = {};
-  visibleNewFeed: boolean = false;
-
-  // Private variable
-  private destroy$ = new Subject<void>();
-
-  constructor(public shareService: ShareService) {}
-  ngOnInit() {
-    this.visibleNewFeed = true;
-    /**
-     * Call API Get New Feeds
-     */
-    const getNewFeed = this.shareService
-      .getNewFeeds()
-      .pipe(takeUntil(this.destroy$));
-    getNewFeed.subscribe({
-      next: (res: any) => {
-        if (res) {
-          this.newFeeds = res.data.newFeeds;
-        }
-      },
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-
-  showDialogNewFeed() {
-    this.visibleNewFeed = true;
-  }
+export class LandingComponent {
+  constructor() {}
+  ngOnInit() {}
 }
