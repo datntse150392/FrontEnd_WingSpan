4ccd5284f10ed8df6e3939d7475bc8351755db89
Nguyen Thanh Dat (DEV) <thuykhangvn@gmail.com>
2024-01-02 03:16:27 +0700

    🐛Config baseUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://ongbutdicodev1.onrender.com/api/',
-  apiUrl: 'http://localhost:5000/api/',
+  apiUrl: 'https://ongbutdicodev1.onrender.com/api/',
+  // apiUrl: 'http://localhost:5000/api/',
 };
